0ebc16a06cf27b6007c1c26268efa93c819cef81
Colin But <colin.but@outlook.com>
2020-12-07 10:59:32 +0000

    feat: basic todo models

--- models.py
+++ models.py
@@ -27,8 +27,43 @@ class Schema:
             _is_done boolean,
             _is_deleted boolean,
             created_on Date DEFAULT CURRENT_DATE,
-            due_data Date,
+            due_date Date,
             user_id INTEGER FOREIGNKEY REFERENCES User(_id)
         )
         """
         self.conn.execute(query)
+
+class ToDoModel:
+    TABLENAME = "TODO"
+
+    def __init__(self):
+        super().__init__()
+        self.conn = sqlite3.connect('todo.db')
+
+    def create(self, text, description, user_id):
+        query = f'insert into {TABLENAME} ' \
+                f'(title, description, user_id) ' \
+                f'values ("{text}","{description}", "{user_id}")'
+
+        result = self.conn.execute(query)
+        return result
+
+    def update(self, text, description):
+        query = f'update {TABLENAME} set title = {text}, description = {description}'
+
+        result = self.conn.execute(query)
+        return result
+
+    def delete(self, id):
+        query = f'update {TABLENAME} set _is_deleted = true'
+
+        result = self.conn.execute(query)
+        return result
+
+    def select(self, user_id):
+        query = f'select * from {TABLENAME} where user_id = "{user_id}"'
+
+        result = self.conn.execute(query)
+
+        return result
+
