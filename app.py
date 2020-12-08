55c53361561c30eaa51504cbeeb560620ca4ba13
Colin But <colin.but@outlook.com>
2020-12-08 23:48:12 +0000

    feat: basic stab at the view

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
-from flask import Flask
+from models import Schema
+from setup import DummyData
+from service import ToDoService
+from flask import Flask, request
+import logging
+
+app = Flask(__name__)
+
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
+
+
+@app.route("/todo", method=["GET"])
+def list_todo():
+    user_id = request.args.get('user_id')
+    LOGGER.info("Retrieving list of todos for user: {}".format(user_id))
+
+    return ToDoService().get_todos(user_id)
+
+
+@app.route("/todo", method=["POST"])
+def create_todo():
+    return ToDoService().create(request.json())
+
+
 
 if __name__ == "__main__":
     Schema()
-    app.run(debug=True)
+    DummyData()
+    app.run(debug=True, host='0.0.0.0', port=8888)
