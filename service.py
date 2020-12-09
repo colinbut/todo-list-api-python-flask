d5c556f000bc993f502b77bad3c9ff8a44772e84
Colin But <colin.but@outlook.com>
2020-12-09 23:43:10 +0000

    feat: new select method to get individual

--- service.py
+++ service.py
@@ -7,13 +7,16 @@ class ToDoService:
         self.model = ToDoModel()
     
     def create_todo(self, params):
-        self.model.create(params["text"], params["description"], 1)
+        self.model.create(params["text"], params["description"], params["user_id"])
 
     def update_todo(self, params):
-        self.model.update(params["text"], params["description"], 1)
+        self.model.update(params["text"], params["description"], params["user_id"])
 
     def delete_todo(self, id):
         self.model.delete(id)
 
     def get_todos(self, user_id):
-        self.model.select(user_id)
+        self.model.select_all(user_id)
+
+    def get_todo(self, id, user_id):
+        self.model.select(id, user_id)
